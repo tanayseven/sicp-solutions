f01e347889ce68aa66e674774ab0c38b3caea481
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-02-25 01:18:23 +0530

    Solution complete for ex_1.3

--- src/ex_1.3.scm
+++ src/ex_1.3.scm
@@ -2,11 +2,14 @@
 ; Define a procedure that takes three numbers as arguments and returns the
 ; sum of the squares of the two larger numbers.
 
+(define (square num)
+  (* num num))
+
 (define (sum-of-square-of-two-larger num1 num2 num3)
-  (define second_largest_1 (if (> num1 num2) 
-                               (num1) 
-                               (num2))
-  (define first_largest (if (> (second_largest_1) num3) 
-                               (second_largest_1) 
-                               ())))
-  )
+(begin
+  (define (second-largest) (if (> num1 num2) 
+                             num1 
+                             num2))
+  (if (> (second-largest) num3)
+      (+ (square num1) (square num2))
+      (+ (square (second-largest)) (square num3)))))
--- tests/test_ex_1.3.scm
+++ tests/test_ex_1.3.scm
@@ -1,10 +1,12 @@
 (load "test-manager/load.scm")
-(load "src/ex_1.2.scm")
+(load "src/ex_1.3.scm")
 
 (in-test-group 
   sum-of-squares-of-two-larger-out-of-three-numbers
   (define-test (if-input-is-2-1-3-output-is-13)
-    (assert-= (sum-of-square-of-two-larger 2 1 3) 13)))
+    (assert-= (sum-of-square-of-two-larger 2 1 3) 13))
+  (define-test (if-input-is-4-6-3-output-is-52)
+    (assert-= (sum-of-square-of-two-larger 4 6 3) 52)))
 
 (run-registered-tests)
 (quit)
