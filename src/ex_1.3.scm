dd394b0baccea8184ac91132c5d8f43e2743e5a6
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-02-24 23:57:39 +0530

    Trying to add logic to the problem

--- src/ex_1.3.scm
+++ src/ex_1.3.scm
@@ -3,4 +3,10 @@
 ; sum of the squares of the two larger numbers.
 
 (define (sum-of-square-of-two-larger num1 num2 num3)
-  (+ (if (and (> num1 num2) (> num1 num3)) (square num1) (if (> num2  num3)))))
+  (define second_largest_1 (if (> num1 num2) 
+                               (num1) 
+                               (num2))
+  (define first_largest (if (> (second_largest_1) num3) 
+                               (second_largest_1) 
+                               ())))
+  )
