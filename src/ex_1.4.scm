0c97d302166bc4bee1fd4cb95397dfe6ab22af22
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-11-28 23:13:24 +0530

    Add a breaking test to fail the CI

---
+++ src/ex_1.4.scm
@@ -0,0 +1,9 @@
+; Exercise 1.3
+; Observe that our model of evaluation allows for combinations whose 
+; operators are compound expressions. Use this observation to describe 
+; the behavior of the following procedure:
+; (define (a-plus-abs-b a b)
+;  ((if (> b 0) + -) a b))
+
+(define (a-plus-abs-b a b)
+  0)
---
+++ tests/test_ex_1.4.scm
@@ -0,0 +1,10 @@
+(load "test-manager/load.scm")
+(load "src/ex_1.4.scm")
+
+(in-test-group 
+  a-plus-abs-b-should-work-the-way-expected
+  (define-test (if-input-is-2-1-output-is-3)
+    (assert-= (a-plus-abs-b 2 1) 3))
+  (define-test (if-input-is-4--3-output-is-1)
+    (assert-= (a-plus-abs-b 4 -3) 1)))
+(run-registered-tests)
