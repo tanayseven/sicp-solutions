eceae446bd194584959cc5ded60512b46b54f995
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-12-07 11:13:57 +0530

    Add logic for script failure on test failure

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
-echo "$1"
-output=$(mit-scheme < $1 | grep "[0-9]* tests, [0-9]* failures, [0-9]* errors")
-echo "$output"
-# TODO add logic to exit with non-zero if there are any failures
-
+echo "\n$1"
+NORMAL='\033[0m'
+RED='\033[0;31m'
+GREEN="\033[32m"
+full_output=$(mit-scheme < $1);
+output=$(echo $full_output | grep -o "[0-9]* tests, [0-9]* failures, [0-9]* errors")
+if [ "$(echo $full_output | grep -o "[0-9]* tests, [1-9]* failures, [0-9]* errors")" = "" ]; then
+    echo "$GREEN$output$NORMAL"
+else
+    echo "$RED$full_output$NORMAL"
+    exit 1
+fi;
